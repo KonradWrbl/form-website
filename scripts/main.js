11052b67112dcd313567199a10fcc23f563a1fb9
KonradWrbl <160815@stud.prz.edu.pl>
2019-03-20 22:26:02 +0100

    IE...

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 $('button').on('click', () => {
     $('#main').toggleClass('container-form');
 })
+
+let IE = () => {
+    if (navigator.appName=="Microsoft Internet Explorer") $("#IE").css('display','inline');
+}
+
+IE();
